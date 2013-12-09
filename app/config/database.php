d5107b3916614c144f8f644f602051cbebc74e15
svpernova09 <halo09@gmail.com>
2013-12-09 17:49:12 -0600

    super sekret database connection. Hopefully no one ever finds this.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'derp_tweeter',
+			'username'  => 'vagrant',
+			'password'  => 'vagrant',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
